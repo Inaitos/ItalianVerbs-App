f2ea839ba7164da2c55f16bccde4c1a152f6eb4f
alexpuh <alexpuh@googlemail.com>
2024-08-05 23:08:17 +0200

    First implementation of menu

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Button, ButtonDirective} from "primeng/button";
+import {MenuModule} from "primeng/menu";
+import {InputGroupModule} from "primeng/inputgroup";
+import {InputGroupAddonModule} from "primeng/inputgroupaddon";
+import {InputTextModule} from "primeng/inputtext";
+import {FormsModule} from "@angular/forms";
+import {MenubarModule} from "primeng/menubar";
+import {MenuItem} from "primeng/api";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonDirective, Button, MenuModule, InputGroupModule, InputGroupAddonModule, InputTextModule, FormsModule, MenubarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Client';
+  protected searchWord?: string;
+  protected menuItems: MenuItem[] = [
+    {
+      label: "Conjugation",
+      items: [
+        {
+          label: 'Top 100',
+        }
+      ]
+    }
+  ];
 }
